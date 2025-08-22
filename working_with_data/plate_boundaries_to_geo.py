5f256310a4b64be716c390532f83a82df102136f
kc-2831 <165983823+kc-2831@users.noreply.github.com>
2025-08-22 19:33:25 +1000

    Delete working_with_data directory

--- working_with_data/plate_boundaries_to_geo.py
+++
@@ -1,8 +0,0 @@
-import geopandas as gpd
-
-gdf = gpd.read_file("Example_Data_From_Client/plates/plate_boundaries_edit.shp")
-print(gdf.head())
-
-# Save it as GeoJSON
-gdf.to_file("plate_boundaries.geojson", driver="GeoJSON")
-
